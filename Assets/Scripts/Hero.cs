60547393940da541ae4562ee58bc2d77bdde50b5
danil614 <fs159@mail.ru>
2022-03-27 15:57:27 +0500

    Hero смещается, но падает, если его не двигать

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Hero : MonoBehaviour, IDragHandler
+public class Hero : MonoBehaviour, IDragHandler, IBeginDragHandler
 {
+    Vector3 startClick;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startClick = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //startClick = //Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //startPoint = transform.position;//Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position += new Vector3(eventData.delta.x, eventData.delta.y);
+        Debug.Log("Start Drag");
+    }
+
     public void OnDrag(PointerEventData eventData)
     {
         //transform.position = eventData.pointerCurrentRaycast.screenPosition;
@@ -11,7 +22,13 @@ public class Hero : MonoBehaviour, IDragHandler
         //eventData.pointerCurrentRaycast.gameObject.transform.position = eventData.pointerCurrentRaycast.screenPosition;
         //GetComponent<Rigidbody2D>().MovePosition(eventData.position);
 
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePosition;
+        //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = mousePosition;
+        Vector3 endClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 clickVector = endClick - startClick;
+        //clickVector -= new Vector2(transform.position.x, transform.position.y);
+        //transform.position += clickVector;
+
+        GetComponent<Rigidbody2D>().MovePosition(clickVector);
     }
 }
