09dcd91c43717a03f27247df62414fed0e3f8c72
danil614 <fs159@mail.ru>
2022-04-25 22:46:12 +0500

    Объединил проекты

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -1,16 +1,17 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-	private Queue<GameObject> objectsOnScene; // Коллекция для хранения объектов на сцене
+	private ConcurrentQueue<GameObject> objectsOnScene; // Коллекция для хранения объектов на сцене
 
 	[SerializeField][Header("Допустимое количество объектов на сцене")] private int allowedNumberCakes;
 	[SerializeField][Header("Пул объектов")] private ObjectPool objectPool;
 
 	private void Awake()
 	{
-		objectsOnScene = new Queue<GameObject>(); // Инициализируем коллекцию
+		objectsOnScene = new ConcurrentQueue<GameObject>(); // Инициализируем коллекцию
 	}
 
 	public void AddToCollection(GameObject gameObject)
@@ -25,7 +26,7 @@ public class Destroyer : MonoBehaviour
     {
 		if (objectsOnScene != null && objectsOnScene.Count > 0)
 		{
-			objectsOnScene.Dequeue(); // Удаляем из очереди
+			objectsOnScene.TryDequeue(out _); // Удаляем из очереди
 		}
 	}
 
@@ -36,8 +37,10 @@ public class Destroyer : MonoBehaviour
 	{
 		if (objectsOnScene != null && objectsOnScene.Count > allowedNumberCakes)
 		{
-			GameObject oldObject = objectsOnScene.Peek(); // Получаем самый старый объект объект
-			GameManager.StartSmoothDestroyer(oldObject, objectPool, 0, 1); // Плавно удаляем со сцены
+			if (objectsOnScene.TryPeek(out GameObject oldObject)) // Получаем самый старый объект объект
+			{
+				GameManager.StartSmoothDestroyer(oldObject, objectPool, 0, 1); // Плавно удаляем со сцены
+			}
 		}
 	}
 }
